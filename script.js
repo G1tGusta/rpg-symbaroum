ab032907005439cef9e72c1dfbb7732712ed5c98
tpz <gustavolucas007@gmail.com>
2024-11-06 03:54:39 -0300

    script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ function toggleTable() {
     const table = document.getElementById("ladinoTable");
     table.style.display = (table.style.display === "none") ? "table" : "none";
 }
-
+function toggleTTable() {
+    const table = document.getElementById("raceTTable");
+    table.style.display = (table.style.display === "none") ? "table" : "none";
+}
