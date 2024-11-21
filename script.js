5f01ff5306fc1685b2e2aaa7dccf2e8ffc64b8fc
tpz <gustavolucas007@gmail.com>
2024-11-21 04:36:44 -0300

    script.js

--- script.js
+++ script.js
@@ -18,3 +18,104 @@ function toggleTTable() {
     table.style.display = (table.style.display === "none") ? "table" : "none";
 }
 
+
+function toggleRainhaTable() {
+    var content = document.getElementById("rainhaTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
+function toggleTerrasTable() {
+    var content = document.getElementById("terrasTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleVidasTable() {
+    var content = document.getElementById("vidasTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function togglePovosTable() {
+    var content = document.getElementById("povosTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleEspititualidadeTable() {
+    var content = document.getElementById("espitualidadeTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleNobresTable() {
+    var content = document.getElementById("nobresTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleBarbarosTable() {
+    var content = document.getElementById("barbarosTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function togglePriosTable() {
+    var content = document.getElementById("priosTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleOrdomagicaTable() {
+    var content = document.getElementById("ordomagicaTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function toggleExercitoRainhaTable() {
+    var content = document.getElementById("exercitorainhaTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function togglePactoFerroTable() {
+    var content = document.getElementById("pactoferroTable");
+    // Verifica se o conteúdo está visível e alterna entre mostrar e esconder
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
